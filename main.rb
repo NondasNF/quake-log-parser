185a5144a87baf8674d5ef934f4373d7e2a37441
Epaminondas Noronha <epaminondas.noronha@codeminer42.com>
2022-01-18 15:31:52 -0300

    fix indentation and bad variable declaration

--- main.rb
+++ main.rb
@@ -1,12 +1,12 @@
-$file_directory = "games.log"
 class Main
-    def readFile
-        if File.exist?($file_directory)
-            quakeLog_firstLine = File.open($file_directory, &:readline) 
-            return puts quakeLog_firstLine
-        else
-            return puts "Files does not exist on main directory"
-        end
+  def read_file
+    if File.exist?("games.log")
+      quakeLog_firstLine = File.open("games.log", &:readline) 
+      return puts quakeLog_firstLine
+    else
+      return puts "Files does not exist on main directory"
     end
+  end
+  
 end
-Main.new.readFile
+Main.new.read_file
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,14 +1,14 @@
-describe "Main" do
-    context "When file exists on main directory", if: File.exist?("games.log") == true do
-        it "Open and read game.log file" do
-            expect(STDOUT).to receive(:puts).with("  0:00 ------------------------------------------------------------\n")
-            require_relative '../main.rb'
-        end
+describe '#read_file' do
+  context "When log file exists", if: File.exist?("games.log") == true do
+    it "Open and read game.log file" do
+      expect(STDOUT).to receive(:puts).with("  0:00 ------------------------------------------------------------\n")
+      require_relative '../main.rb'
     end
-    context "When file don't exists on main directory", if: File.exist?("games.log") == false do
-        it "Failed to open" do
-            expect(STDOUT).to receive(:puts).with("Files does not exist on main directory")
-            require_relative '../main.rb'
-        end
+  end
+  context "When log file don't exists", if: File.exist?("games.log") == false do
+    it "Failed to open" do
+      expect(STDOUT).to receive(:puts).with("Files does not exist on main directory")
+      require_relative '../main.rb'
     end
+  end
 end
