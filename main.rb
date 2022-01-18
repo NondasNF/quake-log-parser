df5cc77eb52d14c6679d725771da84db46db48d7
Epaminondas Noronha <epaminondas.noronha@codeminer42.com>
2022-01-18 04:41:39 -0300

    Update main.rb file with better structured code

--- main.rb
+++ main.rb
@@ -1,5 +1,14 @@
-# quakeLog = File.foreach("games.log") { |line| puts line }
-quakeLog = File.open("games.log") 
-quakeLog_data = quakeLog.readline;
-puts quakeLog_data
-quakeLog.close
+$file_directory = "games.log"
+class Main
+    def readFile
+        if File.exist?($file_directory)
+            quakeLog = File.open($file_directory) 
+            quakeLog_data = quakeLog.readline;
+            quakeLog.close
+            return puts quakeLog_data
+        else
+            return puts "Files does not exist on main directory"
+        end
+    end
+end
+Main.new.readFile
