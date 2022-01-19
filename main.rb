157ea0144d6feb5e90f41bd0fb85ec499801995a
Epaminondas Noronha <epaminondas.noronha@codeminer42.com>
2022-01-19 09:12:11 -0300

    Change the way it prints after conditional

--- main.rb
+++ main.rb
@@ -2,9 +2,9 @@ class Main
   def read_file
     if File.exist?("games.log")
       quakeLog_firstLine = File.open("games.log", &:readline) 
-      return puts quakeLog_firstLine
+      puts quakeLog_firstLine
     else
-      return puts "Files does not exist on main directory"
+      puts "Files does not exist on main directory"
     end
   end
   
