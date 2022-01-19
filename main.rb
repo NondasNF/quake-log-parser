75e28fbf77817c98ab632a7dc5ca7822ab9180bb
Epaminondas Noronha <epaminondas.noronha@codeminer42.com>
2022-01-19 14:55:08 -0300

    add a main line count and reorganized classes

--- main.rb
+++ main.rb
@@ -1,12 +1,34 @@
+require 'json'
+
 class Main
-  def read_file
-    if File.exist?("games.log")
-      quakeLog_firstLine = File.open("games.log", &:readline) 
-      puts quakeLog_firstLine
+  def initialize(file_path)
+    @file_path = file_path
+  end
+  def read_line
+      file = open_file
+      line_first =  file.readline
+      file.close
+      puts line_first
+  end
+  
+  def count_lines
+    file = open_file
+    lines = File.foreach(file).count
+    file.close
+    lines_report = {@file_path =>{:lines => lines}}
+    lines_report_json = lines_report.to_json
+    puts lines_report_json
+  end
+
+  def open_file
+    if File.exist?(@file_path)
+      File.open(@file_path)
     else
       puts "Files does not exist on main directory"
     end
   end
-  
 end
-Main.new.read_file
+
+game_parser = Main.new("games.log")
+game_parser.read_line
+game_parser.count_lines
